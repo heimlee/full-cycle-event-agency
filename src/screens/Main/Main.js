235e28bfddfac54cc12fa3b82842a0d6c8047114
AWDEN\awden <awdenheimlee@gmail.com>
2021-11-06 16:57:35 +0200

    Implement MainFullCycle component

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { MainButtonSwitchLanguages } from './components/mainButtonSwitchLanguages/MainButtonSwitchLanguages';
 import { MainLogo } from './components/mainLogo/MainLogo';
 import { MainButtonQuestion } from './components/mainButtonQuestions/MainButtonQuestion';
+import { MainFullCycle } from './components/mainFullCycle/MainFullCycle';
 
 import './main.scss';
 
@@ -14,7 +15,9 @@ export const Main = () => {
         <MainLogo />
       </header>
       <MainButtonQuestion title="Where?" className="question-where" />
-      <h1>Full-Cycle Event Agency</h1>
+      <section className="main-container__full-cycle-section">
+        <MainFullCycle />
+      </section>
       <MainButtonQuestion title="What?" className="question-what" />
       <footer className="main-container__footer">
         <MainButtonQuestion title="Who?" className="question-who" />
---
+++ src/screens/Main/components/mainFullCycle/MainFullCycle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './mainFullCycle.scss';
+
+export const MainFullCycle = () => {
+  return(
+    <div className='cycle'></div>
+  );
+};
