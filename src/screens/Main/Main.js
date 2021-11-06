ee095dc3ebd362cf0e3b5940b47cd89373dcbb8b
AWDEN\awden <awdenheimlee@gmail.com>
2021-11-06 12:26:50 +0200

    Implement MainButtonSwitchLanguages component

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { MainButtonSwitchLanguages } from './components/mainButtonSwitchLanguages/MainButtonSwitchLanguages';
 import { MainLogo } from './components/mainLogo/MainLogo';
 
 import './main.scss';
@@ -8,6 +9,7 @@ export const Main = () => {
   return(
     <main className="main-container">
       <header className="main-container__header">
+        <MainButtonSwitchLanguages />
         <MainLogo />
       </header>
       <h1>Full-Cycle Event Agency</h1>
---
+++ src/screens/Main/components/mainButtonSwitchLanguages/MainButtonSwitchLanguages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './mainButtonSwitchLanguages.scss';
+
+export const MainButtonSwitchLanguages = () => {
+  return(
+    <div className="button-switch-languages">
+      <div className="eclipse-languages"></div>
+      <div className="icon-languages">👅</div>
+    </div>
+  );
+};
+
