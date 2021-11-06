bfb653f9eca5a9933a32d9d1a85094c9d7574338
AWDEN\awden <awdenheimlee@gmail.com>
2021-11-06 11:43:04 +0200

    Implement MainLogo component

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { MainLogo } from './components/mainLogo/MainLogo';
+
 import './main.scss';
 
 export const Main = () => {
   return(
-    <main>
+    <main className="main-container">
+      <header className="main-container__header">
+        <MainLogo />
+      </header>
       <h1>Full-Cycle Event Agency</h1>
     </main>
   )
