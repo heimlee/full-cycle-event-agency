240b4f0c8549240eb5bf6b6e0b7c037d0ee717ac
AWDEN\awden <awdenheimlee@gmail.com>
2021-11-06 16:37:36 +0200

    Implement MainButtonQuestion component

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { MainButtonSwitchLanguages } from './components/mainButtonSwitchLanguages/MainButtonSwitchLanguages';
 import { MainLogo } from './components/mainLogo/MainLogo';
+import { MainButtonQuestion } from './components/mainButtonQuestions/MainButtonQuestion';
 
 import './main.scss';
 
@@ -12,7 +13,12 @@ export const Main = () => {
         <MainButtonSwitchLanguages />
         <MainLogo />
       </header>
+      <MainButtonQuestion title="Where?" className="question-where" />
       <h1>Full-Cycle Event Agency</h1>
+      <MainButtonQuestion title="What?" className="question-what" />
+      <footer className="main-container__footer">
+        <MainButtonQuestion title="Who?" className="question-who" />
+      </footer>
     </main>
   )
 };
