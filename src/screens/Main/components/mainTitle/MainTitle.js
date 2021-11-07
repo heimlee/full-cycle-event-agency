72792b4c9df688817dda7ee2f561ac9610267ba4
AWDEN\awden <awdenheimlee@gmail.com>
2021-11-07 12:51:50 +0200

    Feat (MainTitle): implement parallax effect for mouse move event

--- src/screens/Main/components/mainTitle/MainTitle.js
+++ src/screens/Main/components/mainTitle/MainTitle.js
@@ -3,7 +3,16 @@ import React from 'react';
 import './mainTitle.scss';
 
 export const MainTitle = () => {
+function handleMouseMove(e) {
+  const x = (window.innerWidth - e.pageX*2)/100;
+  const y = (window.innerHeight - e.pageY*2)/100;
+
+  e.currentTarget.children[0].style.transform = `translate(${x}px, ${y}px)`;
+}
+
   return(
-    <h1 className="main-title">Full-Cycle <br/> Event Agency</h1>
+    <div className="field" onMouseMove={handleMouseMove}>
+      <h1 className="main-title" >Full-Cycle <br/> Event Agency</h1>
+    </div>
   );
 };
