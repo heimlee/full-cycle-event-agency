57a7663f99f9b3a29989200bb62df98f2b9b0d28
AWDEN\awden <awdenheimlee@gmail.com>
2021-11-08 13:28:29 +0200

    Implement MainCircleShowreel component (without scroll effect)

--- src/screens/Main/components/mainFullCycle/MainFullCycle.js
+++ src/screens/Main/components/mainFullCycle/MainFullCycle.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { MainCircleShowreel } from '../mainCircleShowreel/MainCircleShowreel';
+
 import './mainFullCycle.scss';
 
 export const MainFullCycle = () => {
   return(
-    <div className='cycle'></div>
+    <div className="cycle">
+      <div className="inner-circle"></div>
+      <MainCircleShowreel />
+    </div>
   );
 };
